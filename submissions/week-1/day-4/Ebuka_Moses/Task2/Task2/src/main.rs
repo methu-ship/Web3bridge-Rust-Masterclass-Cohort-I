5095ad6bc5e51bd6f847c1f3cee7758398d87b72
Ebuka Moses <okoyemoses83@gmail.com>
2025-07-10 13:49:17 +0100

    Day 4 Task Submission

--- submissions/week-1/day-4/Ebuka_Moses/Task2/Task2/src/main.rs
+++ submissions/week-1/day-4/Ebuka_Moses/Task2/Task2/src/main.rs
@@ -1,4 +1,3 @@
-// Define an enum for box color
 enum BoxColor {
     Red,
     Blue,
@@ -6,7 +5,6 @@ enum BoxColor {
     Yellow,
 }
 
-// Define a struct for the shipping box
 struct ShippingBox {
     length: f32,
     width: f32,
@@ -15,9 +13,8 @@ struct ShippingBox {
     color: BoxColor,
 }
 
-// Implement methods for ShippingBox
 impl ShippingBox {
-    // Constructor method to create a new box
+
     fn new(length: f32, width: f32, height: f32, weight: f32, color: BoxColor) -> Self {
         ShippingBox {
             length,
@@ -28,7 +25,6 @@ impl ShippingBox {
         }
     }
 
-    // Method to print characteristics of the box
     fn print_characteristics(&self) {
         println!("Shipping Box Characteristics:");
         println!("Dimensions: {} x {} x {}", self.length, self.width, self.height);
@@ -36,7 +32,6 @@ impl ShippingBox {
         println!("Color: {}", self.get_color());
     }
 
-    // Helper method to get color as string
     fn get_color(&self) -> &str {
         match self.color {
             BoxColor::Red => "Red",
@@ -48,13 +43,12 @@ impl ShippingBox {
 }
 
 fn main() {
-    // Create a new shipping box for each color
+
     let box_red = ShippingBox::new(30.0, 20.0, 15.0, 5.5, BoxColor::Red);
     let box_blue = ShippingBox::new(25.0, 18.0, 12.0, 4.0, BoxColor::Blue);
     let box_green = ShippingBox::new(35.0, 22.0, 17.0, 6.0, BoxColor::Green);
     let box_yellow = ShippingBox::new(28.0, 19.0, 14.0, 4.8, BoxColor::Yellow);
 
-    // Print their characteristics
     box_red.print_characteristics();
     println!("---");
     box_blue.print_characteristics();
