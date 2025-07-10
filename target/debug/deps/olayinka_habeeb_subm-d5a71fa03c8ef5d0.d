2310b54f544c11cda459c1433b9fa4724f5955ef
methu-ship <mettusella@gmail.com>
2025-07-10 06:28:43 +0100

    feat: Add target/ to .gitignore and untrack build artifacts

--- target/debug/deps/olayinka_habeeb_subm-d5a71fa03c8ef5d0.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Desktop/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Olayinka_Habeeb/olayinka_habeeb_subm/target/debug/deps/olayinka_habeeb_subm-d5a71fa03c8ef5d0: src/main.rs
-
-/Users/mac/Desktop/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Olayinka_Habeeb/olayinka_habeeb_subm/target/debug/deps/olayinka_habeeb_subm-d5a71fa03c8ef5d0.d: src/main.rs
-
-src/main.rs:
