23a6bf72f1865d24f8d63b890fb80818e9071b93
Ijay Abby <inyangasi4@gmail.com>
2025-07-14 12:40:55 +0300

    Remove target

--- submissions/week-1/day-2/Abigael_Nyangasi-Basic_Arithmetic/target/debug/deps/Abigael_Nyangasi_Basic_Arithmetic-1feda846ec0d6793.d
+++
@@ -1,5 +0,0 @@
-/Users/ijayabby/Documents/workspace/Rust Web3bridge/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Abigael_Nyangasi-Basic_Arithmetic/target/debug/deps/Abigael_Nyangasi_Basic_Arithmetic-1feda846ec0d6793.d: src/main.rs
-
-/Users/ijayabby/Documents/workspace/Rust Web3bridge/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Abigael_Nyangasi-Basic_Arithmetic/target/debug/deps/Abigael_Nyangasi_Basic_Arithmetic-1feda846ec0d6793: src/main.rs
-
-src/main.rs:
