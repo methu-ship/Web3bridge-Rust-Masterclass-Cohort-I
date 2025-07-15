178911a6882307f02f6b985211d9ea157178eb4c
signor1 <emmanuelomemgboji@gmail.com>
2025-07-15 11:16:36 +0100

    feat: update

---
+++ submissions/week-1/day-5/omemgboji-emmanuel/task_2/target/debug/deps/task_2-10559a215cb7e2a9.d
@@ -0,0 +1,5 @@
+/Users/mac/Documents/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-5/omemgboji-emmanuel/task_2/target/debug/deps/task_2-10559a215cb7e2a9: src/main.rs
+
+/Users/mac/Documents/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-5/omemgboji-emmanuel/task_2/target/debug/deps/task_2-10559a215cb7e2a9.d: src/main.rs
+
+src/main.rs:
