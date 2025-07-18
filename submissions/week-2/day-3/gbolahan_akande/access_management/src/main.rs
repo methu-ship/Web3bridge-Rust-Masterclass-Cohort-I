f02af6016890c0376d79a9e87b264e410f6c46f4
Gbolahan Akande <geakande@gmail.com>
2025-07-18 11:26:43 +0100

    handling packages and library

--- submissions/week-2/day-3/gbolahan_akande/access_management/src/main.rs
+++ submissions/week-2/day-3/gbolahan_akande/access_management/src/main.rs
@@ -1,68 +1,8 @@
-use std::result;
-
-#[derive(Debug)]
-enum EmployeeType {
-    MediaTeam,
-    ITdept,
-    Managers,
-    SocialMedia,
-    Supervisors,
-    KitchenStaff
-}
-
-#[derive(PartialEq)] 
-#[derive(Debug)]
-enum EmploymentStatus {
-    Employed,
-    NotEmployed
-}
-
-#[derive(Debug)]
-struct EmployeeDetails {
-    employee_id: u32,
-    employee_name: String,
-    employee_type: EmployeeType,
-    is_employed: EmploymentStatus
-}
-
-impl EmployeeDetails {
-
-    fn is_employee_access(&mut self) -> Result<&str, &str> {
-        if self.is_employed == EmploymentStatus::Employed {
-            match self.employee_type {
-                EmployeeType::ITdept => { Ok("You have access") },
-                EmployeeType::KitchenStaff => { Err("No access") },
-                EmployeeType::Managers => { Ok("You have access") },
-                EmployeeType::MediaTeam => { Ok("You have access") },
-                EmployeeType::SocialMedia => { Err("No access") },
-                EmployeeType::Supervisors => { Err("No access") },
-            }
-        } else {
-            Err("You are not Employed")
-        }
-    }
-
-    fn show_access(&mut self) -> Result<&str, &str> {
-        let pass = self.is_employee_access()?;
-        println!("Access Granted {}", pass);
-        Ok(pass)
-    }
-
-    fn print_access(&mut self) -> Result<(), &str> {
-        match self.show_access() {
-            Ok(pass) => {
-                println!("You have access");
-                Ok(())
-            }
-            Err(Fail) => {
-                println!("You have no access");
-                Ok(())
-            }
-        }
-    }
-}
+use access_management::*;
+use access_management::state::*;
 
 fn main() {
+    let test = EmployeeType::ITdept;
     let mut new_access1 = EmployeeDetails {
         employee_id: 1,
         employee_name: String::from("Gbolahan"),
@@ -76,8 +16,8 @@ fn main() {
         employee_type: EmployeeType::ITdept,
         is_employed: EmploymentStatus::NotEmployed
     };
-    let me1 = new_access1.print_access();
-    println!("{:#?}", me1);
-    let me2 = new_access2.print_access();
-    println!("{:#?}", me2);
+    let access1 = print_access(&new_access1);
+    println!("{:#?}", access1);
+    let access2 = print_access(&new_access2);
+    println!("{:#?}", access2);
 }
