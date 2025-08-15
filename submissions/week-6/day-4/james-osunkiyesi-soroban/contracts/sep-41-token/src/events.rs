83178b5e4f85e0c933278dc8265c78a5c61e421b
James Osunkiyesi <jaydhales@gmail.com>
2025-08-15 07:23:57 +0100

    feat: enhance event emission by using IntoVal for data conversion

--- submissions/week-6/day-4/james-osunkiyesi-soroban/contracts/sep-41-token/src/events.rs
+++ submissions/week-6/day-4/james-osunkiyesi-soroban/contracts/sep-41-token/src/events.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{Env, Vec, Symbol, Address, Val};
+use soroban_sdk::{Env, Vec, Symbol, Address, Val, IntoVal};
 
 pub fn emit_approve_event(env: &Env, from: Address, spender: Address, amount: i128, live_until_ledger: u32) {
     let topics: Vec<Val> = Vec::from_array(
@@ -9,7 +9,7 @@ pub fn emit_approve_event(env: &Env, from: Address, spender: Address, amount: i1
             spender.to_val(),
         ],
     );
-    let data: Vec<Val> = Vec::from_array(env, [amount.into(), live_until_ledger.into()]);
+    let data: Vec<Val> = Vec::from_array(env, [amount.into_val(env), live_until_ledger.into_val(env)]);
     env.events().publish(topics, data);
 }
 
