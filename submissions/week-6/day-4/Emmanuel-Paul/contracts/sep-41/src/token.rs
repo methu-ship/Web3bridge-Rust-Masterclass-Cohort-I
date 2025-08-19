e29144e6be57cc8472717640ea84fd50a76f8f51
emmanuelist <emmanuel.paul75@yahoo.com>
2025-08-19 01:05:33 +0100

    feat: implement TokenInterface trait for token operations

---
+++ submissions/week-6/day-4/Emmanuel-Paul/contracts/sep-41/src/token.rs
@@ -0,0 +1,18 @@
+use crate::errors::TokenError;
+use crate::storage::{AllowanceData, Storage};
+use soroban_sdk::{contract, contractimpl, symbol_short, Address, Env, String};
+
+pub trait TokenInterface {
+    fn allowance(env: Env, from: Address, spender: Address) -> i128;
+    fn approve(env: Env, from: Address, spender: Address, amount: i128, expiration_ledger: u32);
+    fn balance(env: Env, id: Address) -> i128;
+    fn transfer(env: Env, from: Address, to: Address, amount: i128);
+    fn transfer_from(env: Env, spender: Address, from: Address, to: Address, amount: i128);
+    fn burn(env: Env, from: Address, amount: i128);
+    fn burn_from(env: Env, spender: Address, from: Address, amount: i128);
+    fn decimals(env: Env) -> u32;
+    fn name(env: Env) -> String;
+    fn symbol(env: Env) -> String;
+    fn initialize(env: Env, admin: Address, decimals: u32, name: String, symbol: String);
+    fn mint(env: Env, to: Address, amount: i128);
+}
